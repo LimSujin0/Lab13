100b8df285c4c20a7cc363444f8fabe2c5c5c526
LimSujin0 <imsujin0@naver.com>
2018-05-02 20:16:02 +0900

    Really Finish

--- src/main/java/edu/handong/csee/java/lab13/prob3/Shape.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Shape.java
@@ -1,9 +1,9 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3;//this is a package
 
-public abstract class Shape {
-	public abstract double area();
-	public abstract double perimeter();
-	public void display() {
-		System.out.println("Area: " + area()+"\nPerimeter: "+perimeter()+"\n");
+public abstract class Shape {//this is a abstract class named Shape.
+	public abstract double area();//this is a abstract method named area
+	public abstract double perimeter();//this is a abstract method named perimeter
+	public void display() {//this is a public method named display
+		System.out.println("Area: " + area()+"\nPerimeter: "+perimeter()+"\n");//print out area and perimeter of the shape on the monitor
 	}
 }
